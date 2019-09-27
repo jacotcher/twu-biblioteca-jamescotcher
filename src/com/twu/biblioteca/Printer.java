0b4d821d7fa65c6fc53606c44d5049310ce31cdc
James Cotcher <jacotcher@gmail.com>
2019-09-27 17:52:27 +0100

    [Setting up testing and maven

---
+++ src/com/twu/biblioteca/Printer.java
@@ -0,0 +1,15 @@
+public class Printer {
+
+    public Printer() {
+    }
+
+    public String print(String message) {
+        System.out.print(message);
+        return message;
+    }
+
+    public String println(String message) {
+        System.out.println(message);
+        return message;
+    }
+}
